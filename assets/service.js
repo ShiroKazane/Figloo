26a92aed376f52637ed8f43c51d9dff762854612
Shiro <bayuofficial773@gmail.com>
2023-11-17 13:48:36 +0700

    Figloo

---
+++ assets/service.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'cache-v2';
+
+self.addEventListener('install', (event) => {
+    console.log('Service worker: Installed.');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service worker: Activated.');
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Service worker: Deleting old caches.');
+                        caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log(`Service worker: Fetching: ${event.request.url}`);
+    event
+        .respondWith(fetch(event.request))
+        .then((res) => {
+            const resClone = res.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+                cache.put(event.request, resClone);
+            });
+            return res;
+        })
+        .catch(() => caches.match(event.request).then((res) => res));
+});
