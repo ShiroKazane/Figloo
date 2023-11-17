8e516ab961ba9cd54cd4a601d98b2665666f3d3f
Shiro <bayuofficial773@gmail.com>
2023-11-17 23:19:05 +0700

    Figloo

--- service.js
+++ service.js
@@ -1,5 +1,4 @@
 const CACHE_NAME = 'cache-v2';
-const LOAD_FLAG = 'c';
 
 self.addEventListener('install', () => {
     self.skipWaiting();
@@ -19,25 +18,11 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then((cachedRes) => {
-        // if (cachedRes) {
-        //     return cachedRes;
-        // }
-        const flagged = !event.request.url.includes(`${LOAD_FLAG}=1`);
-        const requestFlagged = flagged ? new Request(`${event.request.url}?${LOAD_FLAG}=1`, event.request) : event.request;
-        console.log(requestFlagged);
-        return fetch(requestFlagged).then((res) => {
-            caches.open(CACHE_NAME).then((cache) => {
-                cache.put(requestFlagged, res);
-                if (res.status === 200 && flagged) {
-                    clients.matchAll().then((clients) => {
-                        clients.forEach((client) => {
-                            client.postMessage({ action: 'hide' });
-                        });
-                    });
-                }
-                return res.clone();
-            });
+    event.respondWith(fetch(event.request).then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, resClone);
         });
-    }));
+        return res;
+    }).catch(() => caches.match(event.request).then((res) => res)));
 });
