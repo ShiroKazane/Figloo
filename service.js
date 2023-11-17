2a33b26cb1590d8a036aca0eea26d5c0a4bfee7f
Shiro <bayuofficial773@gmail.com>
2023-11-17 22:34:03 +0700

    Figloo

--- service.js
+++ service.js
@@ -1,4 +1,6 @@
 const CACHE_NAME = 'cache-v2';
+const LOAD_FLAG = 'c';
+const CACHED = false;
 
 self.addEventListener('install', () => {
     self.skipWaiting();
@@ -18,11 +20,31 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request).then((res) => {
-        const resClone = res.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, resClone);
-        });
-        return res;
-    }).catch(() => caches.match(event.request).then((res) => res)));
+    event.respondWith(caches.match(event.request).then((cachedRes) => {
+        if (cachedRes) {
+            return cachedRes;
+        }
+        if (!event.request.url.includes(LOAD_FLAG)) {
+            const requestFlagged = new Request(
+                event.request.url + `?${LOAD_FLAG}=1`,
+                event.request
+            );
+            return fetch(requestFlagged).then((res) => {
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(requestFlagged, res);
+                    if (res.status === 200) {
+                        CACHED = true;
+                    }
+                    if (CACHED) {
+                        setTimeout(() => {
+                            const loader = document.querySelector('.load');
+                            loader.classList.add('hide');
+                        }, 1000);
+                    }
+                });
+                return res.clone();
+            });
+        }
+        return fetch(event.request);
+    }));
 });
